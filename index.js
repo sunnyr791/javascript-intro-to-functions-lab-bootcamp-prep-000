57734e41a7d7aa251cc157ec72df5eb3b60fdf57
Sunny Renukuntla <sunnyr791@gmail.com>
2018-07-01 19:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,8 @@ function sayHiToGrandma (string) {
     return "I can't hear you!"
 } else if (string === string.toUpperCase()) {
     return "YES, INDEED!"
-} else if (string === string.toLowerCase())
+} else if (string === "I love you, Gradma") {
     return "I love you too, Grandma!"
+} else
+    return "what was that?"
+}
