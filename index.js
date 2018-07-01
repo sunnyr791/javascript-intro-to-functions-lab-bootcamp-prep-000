121a63b6b01b80976885acfd8b36978f545cefa0
Sunny Renukuntla <sunnyr791@gmail.com>
2018-07-01 19:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  if (string === toLowerCase(string))
+  if (string === string.toLowerCase())
     return "I can't hear you!"
 }
